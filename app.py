074d8c1853ebb60b8ab7643c90c4fdf8515fbc70
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-05-13 15:57:39 -0700

    trying with appy.pod ..

---
+++ app.py
@@ -0,0 +1,107 @@
+from __future__ import with_statement
+
+from flask import Flask, request, abort, send_file
+from redislite import Redis
+from contextlib import closing
+from appy.pod.renderer import Renderer
+import requests
+import logging
+import os
+import tempfile
+import sys
+
+app = Flask("Odt2Pdf")
+redis = Redis("odt2pdf.db")
+# log to stderr
+app.logger.setLevel(logging.WARNING)
+app.logger.addHandler(logging.StreamHandler())
+
+warn = app.logger.warning
+
+APP_KEY = os.environ.get("APP_KEY")
+
+UNOPYTHON = os.environ.get("UNOPYTHON", "")
+
+if not APP_KEY:
+    print("You need to set the APP_KEY environment variable!")
+    sys.exit(1)
+
+def _fetch_template(url=False, headers=None, **kwargs):
+    if not url:
+        abort(400, "Your 'template' doesn't have any url ...")
+
+    headers = dict() if headers is None else headers
+    tmp_name = redis.get("{}".format(url))
+    if redis.get("etag_{}".format(url)):
+        headers['If-None-Match'] = redis.get("etag_{}".format(url))
+
+    with closing(requests.get(url, stream=True, headers=headers, **kwargs)) as r:
+        if r.status_code not in [204, 304]:
+            # if we received a no-content or not-modified, we assume we are still up to date
+            if r.status_code != 200:
+                abort(400, "Received a non 200 response when downloading template: {}".format(r.status))
+
+
+            # we have a new one, download it
+            fd, filename = tempfile.mkstemp(suffix=".odt")
+            for l in r.iter_content(1024):
+                os.write(fd, l)
+
+            os.close(fd)
+
+            # and update the db
+            redis.set("{}".format(url), filename)
+            redis.set("etag_{}".format(url), r.headers.get("etag", None))
+            if tmp_name:
+                try:
+                    os.remove(tmp_name)
+                except:
+                    pass
+            tmp_name = filename
+
+    print(tmp_name)
+    return tmp_name
+
+# accepting mandrill inbound format:
+# https://mandrill.zendesk.com/hc/en-us/articles/205583207-What-is-the-format-of-inbound-email-webhooks-
+
+@app.route('/render/template/{}'.format(APP_KEY), methods=["POST"])
+def render_template():
+    # we are expecting a json payload
+    payload = request.get_json()
+    if not payload:
+        abort(400, "No JSON content found")
+
+    try:
+        context = payload["context"]
+    except KeyError:
+        abort(400, "No context, no document!")
+
+    suffix = payload.get("format", "pdf")
+
+    template = _fetch_template(**payload['template'])
+
+    fd, target = tempfile.mkstemp(suffix=".{}".format(suffix))
+    os.close(fd)
+    os.remove(target)
+
+    try:
+        # FIXME: we are not cleaning up our target documents, are we?
+        r = Renderer(template, context, target,
+                     pythonWithUnoPath=UNOPYTHON,
+                     forceOoCall=True,
+                     overwriteExisting=True)
+        r.run()
+
+        return send_file(target, as_attachment=True)
+    finally:
+        try:
+            os.remove(target)
+        except:
+            pass
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
